27ade3d67a56ef110fba1f32dc151106f27daef4
Arun Venu <v.arun@noxum.com>
2022-08-29 10:27:00 +0530

    Animated sample

--- cybertekwebapp/src/banner/Banner1.tsx
+++ cybertekwebapp/src/banner/Banner1.tsx
@@ -1,11 +1,17 @@
-export const Banner1 = () => {
+export interface IBanner1Props {
+  children?: React.ReactNode;
+}
+export const Banner1 = ({ children }: IBanner1Props) => {
   return (
-    <video
-      className="w-full aspect-video"
-      autoPlay={true}
-      loop={true}
-      muted={true}
-      src={"./videos/video1.mp4"}
-    />
+    <div className="relative">
+      <video
+        className="w-full aspect-video"
+        autoPlay={true}
+        loop={true}
+        muted={true}
+        src={"./videos/video1.mp4"}
+      />
+      {children}
+    </div>
   );
 };
